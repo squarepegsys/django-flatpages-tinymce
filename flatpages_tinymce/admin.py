88461ff8b72cd95ec06bab0d9ec9b34c652d8e14
Mike Hostetler <mike@squarepegsystems.com>
2014-06-17 19:50:34 -0500

    Django 1.6 changed the import of url

--- flatpages_tinymce/admin.py
+++ flatpages_tinymce/admin.py
@@ -10,7 +10,10 @@ from tinymce.widgets import TinyMCE
 from flatpages_tinymce import settings
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, Http404
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls.defaults import patterns, url
+except:
+    from django.conf.urls import patterns, url
 from django.views.decorators.csrf import csrf_protect
 
 
